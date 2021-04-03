2e752e314ad846186db6e41579df21301a21adcc
Delyana Yordanova <delyana.yordanova@outlook.com>
2021-04-03 22:01:19 +0300

    error-strings.js update

--- api/src/common/error-strings.js
+++ api/src/common/error-strings.js
@@ -1,10 +1,12 @@
+import { user as USER } from "./property-length-constraints.js";
+
 export default {
   user: {
-    username: `Expected string with length in the range [2-20]`,
-    password: `Expected string with length in the range [6-20]`,
-    firstName: `Expected string with length in the range [2-20]`,
-    lastName: `Expected string with length in the range [2-20]`,
-    email: `Expected string with length in the range [3-20]`,
+    username: `Expected string with length in the range [${USER.MIN_USERNAME_LENGTH}-${USER.MAX_USERNAME_LENGTH}]`,
+    password: `Expected string with length in the range [${USER.MIN_PASSWORD_LENGTH}-${USER.MAX_PASSWORD_LENGTH}]`,
+    firstName: `Expected string with length in the range [${USER.MIN_FIRSTNAME_LENGTH}-${USER.MAX_FIRSTNAME_LENGTH}]`,
+    lastName: `Expected string with length in the range [${USER.MIN_LASTNAME_LENGTH}-${USER.MAX_LASTNAME_LENGTH}]`,
+    email: `Expected string with length in the range [${USER.MIN_EMAIL_LENGTH}-${USER.MAX_EMAIL_LENGTH}]`,
     age: `Expected number`,
     gender: `Expected "Men", "Women" or "Other"`,
     isBanned: `Expected boolean`,
