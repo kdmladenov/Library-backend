1bbcedf051e6fe756f81af9931ad10d20f666a38
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-04-06 12:39:30 +0300

    updated get books

--- api/src/data/books.js
+++ api/src/data/books.js
@@ -1,11 +1,23 @@
+// dummy data
 export const books = [];
 
 let id = 1;
 
 export const getBooksId = () => id++;
 
+export const createBook = (data) => {
+  const book = {
+    id: books.length + 1,
+    ...data,
+    isBorrowed: false,
+    isDeleted: false,
+  };
+
+  return book;
+};
+
 export const updateBook = (id, isBorrowed) => {
   // 1. Check if book is available in the DB
   // 2. Update isBorrowed value in the DB
   // 3. Write a record in records
-}
+};
--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,15 @@
+/* eslint-disable array-bracket-spacing */
+/* eslint-disable object-curly-newline */
+/* eslint-disable max-len */
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import transformBody from './middleware/transform-body.js';
 import validateBody from './middleware/validate-body.js';
 import createUserScheme from './validator/create-user-schema.js';
+import createBookScheme from './validator/create-book-schema.js';
 import updateBookSchema from './validator/update-book-schema.js';
-import { books, updateBook } from './data/books.js';
+import { books, createBook, updateBook } from './data/books.js';
 import { createUser } from './data/users.js';
 
 const PORT = 5555;
@@ -29,27 +33,44 @@ app.post('/users', transformBody(createUserScheme), validateBody('user', createU
 // USERS - LOGOUT
 
 // BOOKS
+// includes server-side pagination, filtering(searching) or sorting
 app.get('/books', (req, res) => {
-  if (req.query.title) {
-    return res.status(200).json(books.filter(b => b.title.toLowerCase().includes(req.query.title.toLowerCase()) && !b.isDeleted));
+  const { search, sort, take, offset = 0 } = req.query;
+
+  if (search) {
+    return res
+      .status(200)
+      .json(books.filter(b => b.title.toLowerCase().includes(search.toLowerCase()) && !b.isDeleted));
+  }
+
+  if (sort) {
+    const [ sortKey, sortDirection ] = sort.split(',');
+    if (['asc', 'desc'].includes(sortDirection)) {
+      return res
+        .status(200)
+        .json(books
+          .filter(b => !b.isDeleted)
+          .sort((a, b) => a[sortKey].localeCompare(b[sortKey])
+            * ((sortDirection === 'asc') ? 1 : -1)));
+    }
+  }
+
+  if (take && offset >= 0) {
+    return res
+      .status(200)
+      .json(books.filter(b => !b.isDeleted).slice(+offset, +take + (+offset)));
   }
 
   res.status(200).json(books.filter(b => !b.isDeleted));
 });
 
-app.get('/books/:isbn', (req, res) => {
-  const book = books.find(book => book.ISBN === +req.params.isbn && !book.isDeleted);
-  if (!book) {
-    return res.status(404).json({ message: `The book with ISBN ${req.params.isbn} was not found!` });
-  }
-
-  res.status(200).json(book);
-});
-
+/** Get a book by id and ISBN combined */
 app.get('/books/:id', (req, res) => {
-  const book = books.find(book => book.id === +req.params.id && !book.isDeleted);
+  const { id } = req.params;
+  const book = books.find(b => (b.ISBN === +id || b.id === +id) && !b.isDeleted);
+
   if (!book) {
-    return res.status(404).json({ message: `The book with id ${req.params.id} was not found!` });
+    return res.status(404).json({ message: `The book was not found!` });
   }
 
   res.status(200).json(book);
@@ -119,8 +140,10 @@ app.get('/admin/books/:id', (req, res) => {
 });
 
 // create a book
-app.post('/admin/books', (req, res) => {
+app.post('/admin/books', transformBody(createBookScheme), validateBody('book', createBookScheme), (req, res) => {
+  const book = createBook(req.body);
 
+  res.json(book);
 });
 
 // update book by id
