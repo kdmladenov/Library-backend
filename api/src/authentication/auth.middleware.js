9d9fd7b08feea8c9c0e01d46d7eeb7e73756690f
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-04-14 14:56:28 +0300

    Role Middleware

--- api/src/authentication/auth.middleware.js
+++ api/src/authentication/auth.middleware.js
@@ -2,6 +2,17 @@ import passport from 'passport';
 
 const authMiddleware = passport.authenticate('jwt', { session: false });
 
+const roleMiddleware = roleName => (req, res, next) => {
+  if (req.user && req.user.role === roleName) {
+    next();
+  } else {
+    res.status(403).send({
+      message: 'Resource is forbidden.',
+    });
+  }
+};
+
 export {
   authMiddleware,
+  roleMiddleware,
 };
